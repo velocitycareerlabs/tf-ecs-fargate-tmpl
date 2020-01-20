52fac055e88285041c3fe98e7e8d6f328c5cd82a
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-01-20 11:47:48 +0100

    add autoscaling policy for cpu utilization

--- ecs/main.tf
+++ ecs/main.tf
@@ -155,7 +155,8 @@ resource "aws_appautoscaling_target" "ecs_target" {
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "ecs_policy" {
+
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
   name               = "memory-autoscaling"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
@@ -168,6 +169,24 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
     }
 
     target_value       = 80
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+  }
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 60
     scale_in_cooldown  = 300
     scale_out_cooldown = 300
   }
