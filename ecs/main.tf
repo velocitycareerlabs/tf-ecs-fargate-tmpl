405383eca2f6b26fae7463fdecbb0417d4c98d1e
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-01-20 10:15:15 +0100

    add autoscaling to ecs service

--- ecs/main.tf
+++ ecs/main.tf
@@ -141,7 +141,34 @@ resource "aws_ecs_service" "main" {
 
   # we ignore task_definition changes as the revision changes on deploy
   # of a new version of the application
+  # desired_count is ignored as it can change due to autoscaling policy
   lifecycle {
-    ignore_changes = [task_definition]
+    ignore_changes = [task_definition, desired_count]
+  }
+}
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 80
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
   }
 }
