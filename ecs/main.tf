cface20bbd7d607583a4fa852c0edecb60b778df
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-01-23 14:27:19 +0100

    add example for ecsTaskRole

--- ecs/main.tf
+++ ecs/main.tf
@@ -18,9 +18,65 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 EOF
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecsTaskRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "dynamodb" {
+  name        = "${var.name}-task-policy-dynamodb"
+  description = "Policy that allows access to DynamoDB"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:CreateTable",
+                "dynamodb:UpdateTimeToLive",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:ListTables",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem",
+                "dynamodb:UpdateTable"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
     role = aws_iam_role.ecs_task_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+    role       = aws_iam_role.ecs_task_role.name
+    policy_arn = aws_iam_policy.dynamodb.arn
 }
 
 resource "aws_cloudwatch_log_group" "main" {
@@ -39,6 +95,7 @@ resource "aws_ecs_task_definition" "main" {
   cpu                      = var.container_cpu
   memory                   = var.container_memory
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions    = jsonencode([{
       name             = "${var.name}-container-${var.environment}"
       image            = "${var.container_image}:latest"
