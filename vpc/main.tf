7a86dc91051ef3f80684bf7ac1894beef03f770f
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-01-27 11:15:21 +0100

    add vpc flow logs

--- vpc/main.tf
+++ vpc/main.tf
@@ -109,6 +109,61 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_flow_log" "main" {
+  iam_role_arn    = aws_iam_role.vpc-flow-logs-role.arn
+  log_destination = aws_cloudwatch_log_group.main.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name = "${var.name}-cloudwatch-log-group"
+}
+
+resource "aws_iam_role" "vpc-flow-logs-role" {
+  name = "${var.name}-vpc-flow-logs-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "vpc-flow-logs-policy" {
+  name = "${var.name}-vpc-flow-logs-policy"
+  role = aws_iam_role.vpc-flow-logs-role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 output "id" {
   value = aws_vpc.main.id
 }
